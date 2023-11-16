4d90482e16651dbc4ba196f0ddb3fc99de3771ad
idutsu <email@idutsu.com>
2023-11-16 01:22:22 +0000

    third commit

--- kirikuchikun.py
+++ kirikuchikun.py
@@ -1,8 +1,10 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    secret_key = os.environ.get('OPENAI_SECRET_KEY');
     return 'Welcome to my website!'
 
 if __name__ == '__main__':
